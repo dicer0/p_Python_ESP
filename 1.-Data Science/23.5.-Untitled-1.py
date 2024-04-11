e9c1fa649ce7e9128ab830e387d2aa78179b6c2a
“di_cer0” <dcervantesr1401@alumno.ipn.mx>
2024-04-10 19:44:52 -0600

    MechaBible Mark I: Programming - Python: Arduino GUI Multiple Windows, Database Connection, Data Analysis & Static Data Addition

--- 1.-Data Science/23.5.-Untitled-1.py
+++ 1.-Data Science/23.5.-Untitled-1.py
@@ -10,11 +10,24 @@ def copy_excel_data_to_clipboard(file_path, delay=5):
         sheet.range('A1').select()
         sheet.api.UsedRange.Copy()
     
-    # Espera un tiempo antes de cerrar el libro de Excel
-    time.sleep(delay)
+    # Countdown timer
+    for remaining in range(delay, 0, -1):
+        print(f"Countdown: {remaining} seconds", end="\r")
+        time.sleep(1)
     
-    # Cierra el libro de Excel después de copiar los datos
-    wb.close()
+    print("Countdown finished. Closing Excel...")
+    
+    # Try to close the Excel file
+    try:
+        # Set DisplayAlerts to False to suppress pop-up messages
+        wb.app.display_alerts = False
+        wb.close()
+    except Exception as e:
+        print(f"An error occurred while closing the Excel file: {e}")
+    
+    # Quit the Excel application
+    for app in xw.apps:
+        app.quit()
 
 # Ejemplo de uso
 excelFilePath2 = "C:/Users/diego/OneDrive/Documents/The_MechaBible/p_Python_ESP/1.-Data Science/0.-Archivos_Ejercicios_Python/23.-GUI PyQt5 Conexion DataBase/23.-Reporte Analisis de Datos 2.xlsx"
