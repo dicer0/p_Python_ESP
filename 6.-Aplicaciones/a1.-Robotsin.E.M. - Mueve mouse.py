2afd2d95c56403fa67d6789876e2de11e913e5f1
“di_cer0” <dcervantesr1401@alumno.ipn.mx>
2024-04-27 10:14:44 -0600

    MechaBible Mark I: Programming - Python: Arduino GUI Multiple Windows, Database Connection, Data Analysis, Static Data Addition, Excell Cell Auto Adjustment & Excel Data Copy

--- 6.-Aplicaciones/a1.-Robotsin.E.M. - Mueve mouse.py
+++ 6.-Aplicaciones/a1.-Robotsin.E.M. - Mueve mouse.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
+
+#En Python se introducen comentarios de una sola linea con el simbolo #.
+#La primera línea de código incluida en este programa se conoce como declaración de codificación o codificación 
+#de caracteres. Al especificar utf-8 (caracteres Unicode) como la codificación, nos aseguramos de que el archivo 
+#pueda contener caracteres especiales, letras acentuadas y otros caracteres no ASCII sin problemas, garantizando 
+#que Python interprete correctamente esos caracteres y evite posibles errores de codificación.
+#Se puede detener una ejecución con el comando [CTRL] + C puesto en consola, con el comando "cls" se borra su 
+#historial y en Visual Studio Code con el botón superior derecho de Play se corre el programa.
+#Para comentar en Visual Studio Code varias líneas de código se debe pulsar:
+#[CTRL] + K (VSCode queda a la espera). Después pulsa [CTRL] + C para comentar y [CTRL] + U para descomentar.
+
 import pyautogui
 import keyboard
 #math: Librería que proporciona funciones y constantes matemáticas como seno, π, logaritmo, etc.
