7f663c2331a5b7bff0516a85afcf125f687d75a8
“di_cer0” <dcervantesr1401@alumno.ipn.mx>
2024-05-04 11:36:42 -0600

    MechaBible Mark I: Programming - Python: Arduino GUI Multiple Windows, Database Connection, Data Analysis, Static Data Addition, Excell Cell Auto Adjustment & Excel Data Copy

--- 6.-Aplicaciones/a1.-Robotsin.E.M. - Mueve mouse.py
+++ 6.-Aplicaciones/a1.-Robotsin.E.M. - Mueve mouse.py
@@ -19,14 +19,14 @@ import math
 #time: Librería para el manejo de tiempos, como retardos, contadores, etc.
 import time
 
-# Get the screen resolution
+#Get the screen resolution
 screen_width, screen_height = pyautogui.size()
-# Calculate the center of the screen
+#Calculate the center of the screen
 center_x = screen_width // 2
 center_y = screen_height // 2
-# Set the radius of the circular motion
+#Set the radius of the circular motion
 radius = 100
-# Set the speed of the circular motion (adjust as needed)
+#Set the speed of the circular motion (adjust as needed)
 speed = 0.05  # Adjust this value to control the speed
 
 #calculate_next_position(): Función que calcula la próxima posición del mouse.
@@ -34,16 +34,16 @@ def calculate_next_position(angle):
     x = center_x + int(radius * math.cos(angle))
     y = center_y + int(radius * math.sin(angle))
     return x, y
-# Main loop
+#Main loop
 angle = 0
 while not keyboard.is_pressed('s'):
-    # Calculate the next position
+    #Calculate the next position
     next_x, next_y = calculate_next_position(angle)
-    # Move the mouse to the next position
+    #Move the mouse to the next position
     pyautogui.moveTo(next_x, next_y, duration=0)
-    # Increment the angle
+    #Increment the angle
     angle += speed
-    # Wrap angle to keep it within 0 to 2*pi
+    #Wrap angle to keep it within 0 to 2*pi
     angle %= (2 * math.pi)
     
     #time.sleep(): Método que se utiliza para suspender la ejecución de un programa durante un intervalo 
