21380caab056a117fca671bcb7e7ce3a5adba9c7
“di_cer0” <dcervantesr1401@alumno.ipn.mx>
2024-06-18 02:25:47 -0600

    MechaBible Mark I: Programming - Python: Arduino GUI Multiple Windows, Database Connection, Data Analysis, Static Data Addition, Excell Cell Auto Adjustment & Excel Data Copy

--- 1.-Data Science/z23_7_Untitled.py
+++ 1.-Data Science/z23_7_Untitled.py
@@ -2,7 +2,7 @@ import win32serviceutil
 import win32service
 import win32event
 import time
-import os
+import subprocess
 from datetime import datetime
 
 class MyService(win32serviceutil.ServiceFramework):
@@ -21,23 +21,42 @@ class MyService(win32serviceutil.ServiceFramework):
         win32event.SetEvent(self.hWaitStop)
 
     def SvcDoRun(self):
+        print("Service is running...")
         while self.running:
             self.check_time_and_run_script()
-            time.sleep(60)  # Check every minute
+            time.sleep(30)  # Check every 30 seconds
 
     def check_time_and_run_script(self):
         # Especifica la hora en la que quieres ejecutar el script
-        target_hour = 2  # Ejemplo: 14 para las 2:00 PM
-        target_minute = 3
+        target_hour = 2  # Ejemplo: 2 para las 2:00 AM (hora en formato de 24 horas)
+        target_minute = 27  # Ejemplo: 18 para los 18 minutos
 
         now = datetime.now()
+        print(f"Current time: {now.hour}:{now.minute}")
+
         if now.hour == target_hour and now.minute == target_minute:
+            print(f"Executing script at {target_hour}:{target_minute}")
             self.run_script()
+        else:
+            print(f"Waiting for {target_hour}:{target_minute}...")
 
     def run_script(self):
         # Ruta del script Python a ejecutar
-        script_path = r"C:/Users/diego/OneDrive/Documents/The_MechaBible/p_Python_ESP/23.4.-POO GUI Multiples Pantallas PyQt5.py"
-        os.system(f"python {script_path}")
+        script_path = "C:/Users/diego/OneDrive/Documents/The_MechaBible/p_Python_ESP/23.4.-POO GUI Multiples Pantallas PyQt5.py"
+        
+        try:
+            # Ejecutar el script en un proceso separado
+            subprocess.Popen(["pythonw", script_path])
+            print(f"GUI iniciada: {script_path}")
+
+            # Esperar 30 segundos antes de cerrar el proceso
+            time.sleep(30)
+
+            # Terminar el proceso de la GUI después de 30 segundos
+            subprocess.Popen(["taskkill", "/f", "/im", "pythonw.exe", "/t"])
+            print("GUI cerrada")
+        except Exception as e:
+            print(f"Error al ejecutar la GUI: {e}")
 
 if __name__ == '__main__':
     win32serviceutil.HandleCommandLine(MyService)
