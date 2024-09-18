f0d37d7c365c33b8fc8bd81031f8cc099a6f06d9
“di_cer0” <dcervantesr1401@alumno.ipn.mx>
2024-09-18 11:31:48 -0600

    MechaBible Mark I: Programming - Python: Artificial Intelligence FastAPI usage

--- 5.-Inteligencia Artificial/16.-Untitled-1.py
+++ 5.-Inteligencia Artificial/16.-Untitled-1.py
@@ -1,6 +1,4 @@
-
 from POO_API_AsistenteVirtual import ChatGPT_TypeNotation
-
 from API_Keys.Llaves_ChatGPT_Bard_etc import LlaveChatGPT
 
 import asyncio
@@ -13,17 +11,8 @@ app = fastapi.FastAPI()
 @app.get("/stream-example")
 async def stream_example():
     async def stream_tokens():
-        """
-        Placeholder implementation for token streaming. Try running this route as-is to better understand how to
-        stream data using Server-Sent Events (SSEs) in FastAPI.
-        See this tutorial for more information: https://devdojo.com/bobbyiliev/how-to-use-server-sent-events-sse-with-fastapi
-        """
         for token in ['hello', ', ', 'this ', 'is ', 'a ', 'streamed ', 'response.']:
-            # fake delay:
             await asyncio.sleep(random.randint(0, 3))
-
             print(f"Yielding token: {token}")
             yield token
-
     return sse_starlette.sse.EventSourceResponse(stream_tokens())
-# Your code/routes here (you may also keep code in separate files and import/it them here):
