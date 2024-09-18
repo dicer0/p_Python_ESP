dddc1c07ce6ab48ba2df196a9a38bb64aa0e8425
“di_cer0” <dcervantesr1401@alumno.ipn.mx>
2024-09-18 11:17:49 -0600

    MechaBible Mark I: Programming - Python: Artificial Intelligence FastAPI usage

--- 5.-Inteligencia Artificial/z16_FastAPI.py
+++ 5.-Inteligencia Artificial/z16_FastAPI.py
@@ -26,7 +26,7 @@ import random
 #FastAPI: Es una bilbioteca para crear APIs web rápidas y eficientes. Está basada en la librería Starlette para la 
 #gestión de solicitudes HTTP y la librería Pydantic para la validación de datos, lo que la convierte en una opción 
 #ideal para construir APIs modernas y robustas. Sus mayores ventajas son:
-# - Documentación Automática: Mientras se construyen los endpoints de la API (las carpetas o rutas de la URL) usando 
+# - Documentación Automática: Mientras se construyan los endpoints de la API (las carpetas o rutas de la URL) usando 
 #   los 4 métodos HTTP que conforman el CRUD (Create, Read, Update y Delete): GET, POST, PUT y DELETE. Se estará 
 #   creando automáticamente una documentación de su uso al acceder al endpoint URL/docs.
 #       - Create -> POST: Método HTTP que permite mandar datos al endpoint de una API.
@@ -35,20 +35,58 @@ import random
 #       - Delete -> DELETE: Método HTTP que permite borrar los datos previamente almacenados en un endpoint.
 # - Validación de Datos: Estos se realizan a través de la librería Pydantic, la cual realiza operaciones de anotación 
 #   de datos con la siguiente sintaxis: nombreAtributo: Tipo de dato = "Valor por default".
+#Para crear las conexiones con FastAPI se tienen que realizar las siguientes instalaciones:
+#pip install fastapi: La API que permite utilizar el servidor de Uvicorn.
+#pip install uvicorn: Uvicorn es un servidor ASGI (Asynchronous Server Gateway Interface) y es un estándar para la 
+#interfaz entre servidores web y aplicaciones web en Python, permitiendo así manejar solicitudes asíncronas de manera 
+#eficiente. Está diseñado para ser compatible con aplicaciones web modernas que requieren manejo de operaciones 
+#asíncronas, como websockets y aplicaciones en tiempo real.
+#Para levantar el servidor de FastAPI se debe utilizar el siguiente comando en consola:
+#   uvicorn nombreArchivoPython:nombreInstanciaFastAPI --reload
 import fastapi
 #sse_starlette: Esta librería se utiliza en aplicaciones web que usen la bilbioteca FastAPI para habilitar el soporte 
 #de Server-Sent Events (SSE); tecnología que permite al servidor enviar actualizaciones automáticas al cliente a través 
 #de una conexión HTTP persistente, lo que facilita la transmisión continua de datos en tiempo real.
 import sse_starlette
 
-
+#fastapi.FastAPI(): Con esto se crea una instancia de la librería FastAPI para poder crear las rutas (endpoints) de una 
+#API y así manejar las diferentes solicitudes HTTP específicas que conforman el CRUD: Create, Read, Update y Delete.
+#Esta instancia se utiliza en el siguiente comando de consola que sirve para levantar el servidor:
+#   uvicorn nombreArchivoPython:nombreInstanciaFastAPI --reload
+#       - uvicorn: Es el servidor de tipo ASGI que utiliza la librería FastAPI.
+#       - nombreArchivoPython: Es el nombre de este archivo Python, el cual no puede empezar con un número y no puede 
+#         contener caracteres especiales más que el guión bajo.
+#       - nombreInstanciaFastAPI: Es el nombre de la instancia de FastAPI dentro de este archivo Python.
+#       - Comandos de configuración de servidor, estos son algunos de ellos:
+#           --host: Especifica la dirección IP en la que el servidor escuchará. Por defecto es 127.0.0.1.
+#           --port: Define el puerto en el que el servidor escuchará. Por defecto es 8000.
+#           --reload: Habilita la recarga automática del servidor cuando detecta cambios en el código. Ideal para 
+#           desarrollo.
+#           --workers: Número de procesos de trabajo a usar. Aumentar el número de trabajadores puede mejorar el 
+#           rendimiento en producción. 
+#Para ver el resultado de la API, debemos acceder al siguiente URL a través de nuestro navegador: http://127.0.0.1:8000
+#Sobre este URL es que se podrá ver el resultado de los diferentes endpoints de la API: http://127.0.0.1:8000/EndPoints
 app = fastapi.FastAPI()
 
+#Métodos HTTP: Sobre la instancia de FastAPI que ha levantado el servidor Uvicorn se pueden ejecutar funciones propias 
+#síncronas y asíncronas que apliquen los siguientes métodos HTTP sobre los distintos endpoints creados en esta URL: 
+#http://127.0.0.1:8000/EndPoints
+#Los métodos HTTP de la API creada con la librería FastAPI crearán una documentación automática al utilizarse: 
+# - Documentación Automática: Mientras se construyan los endpoints de la API (las carpetas o rutas de la URL) usando 
+#   los 4 métodos HTTP que conforman el CRUD (Create, Read, Update y Delete): GET, POST, PUT y DELETE. Se estará 
+#   creando automáticamente una documentación de su uso al acceder al endpoint URL/docs.
+#       - Create -> POST: Método HTTP que permite mandar datos al endpoint de una API.
+#       - Read ->   GET: Método HTTP que permite obtener los datos almacenados en algún endpoint de una API.
+#       - Update -> PUT: Método HTTP que permite actualizar los datos previamente almacenados en un endpoint.
+#       - Delete -> DELETE: Método HTTP que permite borrar los datos previamente almacenados en un endpoint.
+#@: Esto se llama decorador y su función es la de tomar un método y agregarle una funcionalidad extra. En específico la 
+#función del decorador al utilizarse junto la instancia de FastAPI es la de indicarle al programa la acción a ejecutar 
+#al acceder a cierto endpoint.
 @app.get("/stream-example")
+#stream_example(): Función asíncrona propia que sirve para transmitir texto en un chat donde se lleva una conversación 
+#entre usuario y un asistente virtual de ChatGPT, en particular transmitiendo texto del servidor al cliente mandando 
+#una palabra o token a la vez. 
 async def stream_example():
-    """
-    Example route for streaming text to the client, one word/token at a time.
-    """
     async def stream_tokens():
         """
         Placeholder implementation for token streaming. Try running this route as-is to better understand how to
